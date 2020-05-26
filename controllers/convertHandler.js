41f772cf5dbdfd7df82594e9dfaa6c3bdb2b7fe2
f_akmal <f_akmal@hotmail.com>
2020-05-27 02:00:46 +0800

    first commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,46 +7,60 @@
 */
 
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
-  };
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    var result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
-  };
-  
+  this.getNum = function (input) {
+    const pattern = /(?<value>[0-9\.\/]*)(?<unit>[A-z]*)/
+    const value = input.match(pattern).groups.value
+
+    if (value === '') return 1
+
+    if ((value.match(/\./g) || []).length > 1) return 'invalid number'
+
+    const result = value.split('/').map(parseFloat).reduce((x, y) => x / y)
+
+    if (isNaN(result)) return 'invalid number'
+
+    return result
+  }
+
+  this.getUnit = function (input) {
+    const units = ['gal', 'l', 'mi', 'km', 'lbs', 'kg', 'GAL', 'L', 'MI', 'KM', 'LBS', 'KG']
+    const pattern = /(?<value>[0-9\.\/]*)(?<unit>[A-z]*)/
+    const result = input.match(pattern).groups.unit
+    return units.includes(result) ? result : 'invalid unit'
+  }
+
+  this.getReturnUnit = function (initUnit) {
+    const input = ['gal', 'l', 'mi', 'km', 'lbs', 'kg']
+    const expect = ['l', 'gal', 'km', 'mi', 'kg', 'lbs']
+    const map = input.map((i, index) => ({ [i]: expect[index] }))
+      .reduce((obj, x) => Object.assign(obj, { ...x }))
+    return map[initUnit.toLowerCase()]
+  }
+
+  this.spellOutUnit = function (unit) {
+    const input = ['gal', 'l', 'mi', 'km', 'lbs', 'kg']
+    const expect = ['gallons', 'litres', 'miles', 'kilometres', 'pounds', 'kilograms']
+    const map = input.map((i, index) => ({ [i]: expect[index] }))
+      .reduce((obj, x) => Object.assign(obj, { ...x }))
+    return map[unit.toLowerCase()]
+  }
+
+  this.convert = function (initNum, initUnit) {
+    const galToL = 3.78541
+    const lbsToKg = 0.453592
+    const miToKm = 1.60934
+    var result
+
+    return result
+  }
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    var result
+
+    return result
+  }
+
 }
 
-module.exports = ConvertHandler;
+module.exports = ConvertHandler
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,55 +6,105 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+var chaiHttp = require('chai-http')
+var chai = require('chai')
+var assert = chai.assert
+var server = require('../server')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
 
-  suite('Routing Tests', function() {
-    
-    suite('GET /api/convert => conversion object', function() {
-      
-      test('Convert 10L (valid input)', function(done) {
-       chai.request(server)
-        .get('/api/convert')
-        .query({input: '10L'})
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
-          done();
-        });
-      });
-      
-      test('Convert 32g (invalid input unit)', function(done) {
-        
-        //done();
-      });
-      
-      test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        
-        //done();
-      });  
-      
-      test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
-        //done();
-      });
-      
-      test('Convert kg (no number)', function(done) {
-        
-        //done();
-      });
-      
-    });
+  suite('Routing Tests', function () {
 
-  });
+    suite('GET /api/convert => conversion object', function () {
 
-});
+      test('Convert 10L (valid input)', function (done) {
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: '10L' })
+          .end(function (err, res) {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 10)
+            assert.equal(res.body.initUnit, 'L')
+            assert.approximately(res.body.returnNum, 2.64172, 0.1)
+            assert.equal(res.body.returnUnit, 'gal')
+            done()
+          })
+      })
+
+      test('Convert 32g (invalid input unit)', function (done) {
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: '32g' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 32)
+            assert.equal(res.body.initUnit, 'g')
+            assert.equal(res.body.returnNum, 32)
+            assert.equal(res.body.returnUnit, 'invalid unit')
+            done()
+          })
+      })
+
+      test('Convert 3/7.2/4kg (double fraction)', function (done) {
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: '3/7.2/4kg' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.approximately(res.body.initNum, 0.10417, 0.1)
+            assert.equal(res.body.initUnit, 'kg')
+            assert.approximately(res.body.returnNum, 0.02966, 0.1)
+            assert.equal(res.body.returnUnit, 'lbs')
+            done()
+          })
+      })
+
+      test('Convert 6.2.2L (invalid number)', function (done) {
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: '6.2.2L' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 'invalid number')
+            assert.equal(res.body.initUnit, 'L')
+            assert.equal(res.body.returnNum, 'invalid number')
+            assert.equal(res.body.returnUnit, 'gal')
+            done()
+          })
+      })
+
+      test('Convert 6.2.2kilomegagram (invalid number and unit)', function (done) {
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: '6.2.2kilomegagram' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 'invalid number')
+            assert.equal(res.body.initUnit, 'invalid unit')
+            assert.equal(res.body.returnNum, 'invalid number')
+            assert.equal(res.body.returnUnit, 'invalid unit')
+            done()
+          })
+      })
+
+      test('Convert kg (no number)', function (done) {
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: 'kg' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 1)
+            assert.equal(res.body.initUnit, 'kg')
+            assert.approximately(res.body.returnNum, 2.20462, 0.1)
+            assert.equal(res.body.returnUnit, 'lbs')
+            done()
+          })
+      })
+
+    })
+
+  })
+
+})
